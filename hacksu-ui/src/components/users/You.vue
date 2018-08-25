70d6b03ffe53a74f08835e5b97e9cb71f29c495f
Ben Holland <bhollan5@kent.edu>
2018-08-25 15:46:21 -0400

    Finished image uploading

--- hacksu-ui/src/components/users/You.vue
+++ hacksu-ui/src/components/users/You.vue
@@ -1,26 +1,44 @@
 <template>
 <div class="full-page" id="you">
-  <div class="current-status">
-   
+  <h1>Your Profile</h1>
+  <div class="details">
     
-    <div class="picture-upload"
-         @click="$refs.profileUpload.click()">
+    <div class="edit-details">
+      <p>Edit your profile:</p>
+      <div class="image-uploads">
+        <div>
+          <div class="picture-upload"
+               @click="$refs.profileUpload.click()">
+            
+            <input type="file" ref="profileUpload" @change="onProfilePicSelected">
+            
+            <img class="profile-picture" v-if="$parent.user && $parent.user.profilePicture" 
+                v-bind:src="$parent.user.profilePicture">
+            <img class="profile-picture" v-else 
+                src="../../../static/default-profile-pic.png">
+            <p>Click to Change</p>
+          </div>
+          <p v-if="!loadingProfile">Profile picture</p>
+          <p v-else>Updating...</p>
+        </div>
       
-      <input type="file" ref="profileUpload" @change="onProfilePicSelected">
+        <div>
+          <div class="picture-upload"
+               @click="$refs.hoverUpload.click()">
+            
+            <input type="file" ref="hoverUpload" @change="onHoverPicSelected">
+            
+            <img class="profile-picture" v-if="$parent.user && $parent.user.hoverPicture" 
+                v-bind:src="$parent.user.hoverPicture">
+            <img class="profile-picture" v-else 
+                src="../../../static/default-hover-pic.png">
+            <p>Click to Change</p>
+          </div>
+          <p v-if="!loadingHover">Hover picture</p>
+          <p v-else>Updating...</p>
+        </div>
+      </div>
       
-      <img class="profile-picture" v-if="$parent.user && $parent.user.profilePicture" 
-          v-bind:src="$parent.user.profilePicture">
-      <img class="profile-picture" v-else 
-          src="../../../static/default-profile-pic.png">
-      <p>Click to Change</p>
-    </div>
-    
-    <div class="picture-upload">
-      <img class="profile-picture" v-if="$parent.user && $parent.user.hoverPicture" 
-          v-bind:src="$parent.user.hoverPicture">
-      <img class="profile-picture" v-else 
-          src="../../../static/default-hover-pic.png">
-      <p>Click to Change</p>
     </div>
     
   </div>
@@ -35,15 +53,59 @@ export default {
   data() {
     return {
       selectedProfilePic: null,
+      loadingProfile: false,
+      loadingHover: false,
     }
   }, 
   methods: {
     onProfilePicSelected(event) {
       var file = event.target.files[0];
-      console.log(event);
+      this.loadingProfile = true;
+      this.uploadImage(file, 'profile')
+    },
+    
+    onHoverPicSelected(event) {
+      var file = event.target.files[0];
+      this.loadingHover = true;
+      this.uploadImage(file, 'hover')
+    },
+    
+    // Type should either be 'profile' or 'hover'
+    uploadImage(file, type) {
       
       var storageRef = firebase.storage().ref();
-    }
+      var locationStr = this.$parent.user.id + '/' + type + 'Pic.jpg';
+      console.log("Locationstr: ", locationStr);
+      var ref = storageRef.child(locationStr);
+      
+      var vm = this;
+      
+      // Uploading file to the database: 
+      ref.put(file).then(function(snapshot) {
+        console.log('Uploaded a file!', snapshot);
+        
+        var updateObj = {}; // Declaring this out here so we can set whether we're
+                            // updating 'profilePicture' or 'hoverPicture'
+        updateObj[type + 'Picture'] = snapshot.downloadURL;
+        
+        // Updating database's image:
+        vm.$parent.db.collection('users').doc(vm.$parent.user.id)
+        .update(updateObj).then(() =>{
+          // Setting current session's image:
+          vm.$parent.user[type + 'Picture'] = snapshot.downloadURL;
+          // Ending loading:
+          vm.loadingProfile = false;
+          vm.loadingHover = false;
+          
+        }).catch((err) => {
+          console.error("Error updating your " + type + " image: ", err);
+        })
+        
+      }).catch((err) => {
+        console.error("Error uploading your " + type + " pic: ", err);
+      })
+    },
+    
   }
 }
 </script>
@@ -54,11 +116,30 @@ export default {
   }
   #you {
     background: var(--bg-black);
+/*    padding: 50px 5vw 200px 5vw;*/
+    width: 100vw;
+    
+    color: white;
+  }
+  h1 {
+    font-size: 5rem;
+    text-align: left;
+    display: block;
+    padding: 50px 5vw 30px 5vw;
+    background: var(--light-bg);
+  }
+  
+  .details {
+    display: flex;
     padding: 50px 5vw 200px 5vw;
   }
-  .current-status {
+  .edit-details {
+    border: solid 2px var(--green);
+  }
+  .image-uploads {
     display: flex;
   }
+  
   .picture-upload {
     color: white;
   }
@@ -76,13 +157,14 @@ export default {
     width: 200px;
     height: 200px;
     margin-right: 25px;
+    margin-left: 25px;
   }
   .picture-upload p {
     display: none;
   }
   .picture-upload:hover p {
     display: block;
-    position: relative;
+    position: absolute;
     width: 100%;
     text-align: center;
     cursor: pointer;
